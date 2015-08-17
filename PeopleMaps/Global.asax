7a0a7741f740f57cc0baf9043b69e71680f845f8
Satish Yadav <monstersatish@live.com>
2015-08-17 21:52:27 +0530

    Init

---
+++ PeopleMaps/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PeopleMaps.MvcApplication" Language="C#" %>
---
+++ PeopleMaps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PeopleMaps.Startup))]
+namespace PeopleMaps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
